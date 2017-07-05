38afca93b80a76833d4ec4efc19d910c0677fdcb
Marcelo <marbarrerach@hotmail.com>
2017-07-05 16:07:41 +0200

    transitions support in about page

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49965,6 +49965,21 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
+	statics: {
+	willTransitionTo: function(transition, params, query, callback) {
+		if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+			transition.abort();
+		} else {
+			callback();
+		}
+	},
+	
+	willTransitionFrom: function(transition, component) {
+		if (!confirm('Are you sure you want to leave a page that\'s this exciting?')) {
+			transition.abort();
+		}
+	}
+	},
 	render: function () {
 		return (
 			React.createElement("div", null, 
