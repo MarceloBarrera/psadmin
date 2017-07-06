ebb09386fc3884b0fed542f68a45a3c442b350d8
Marcelo Barrera <marcelo.barrera@ntrans.com>
2017-07-06 11:37:47 +0200

    creating a form and managing state simple example

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,16 +1,33 @@
 "use strict";
 
 var React = require('react');
+
+var AuthorForm = require('./authorForm');
+
 // var Router = require('react-router');
-// var AuthorForm = require('./authorForm');
 // var AuthorApi = require('../../api/authorApi');
 // var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({	
+	getInitialState: function() {
+		return {
+			author: { id: '', firstName: '', lastName: '' }			
+		};
+	},
+
+	setAuthorState: function(event) {		
+		var field = event.target.name;
+		var value = event.target.value;
+		this.state.author[field] = value;
+		return this.setState({author: this.state.author});
+	},
 
 	render: function() {
 		return (
-            <h1>Manage Author </h1>
+			<div>			
+				<AuthorForm author={this.state.author}
+				onChange={this.setAuthorState} />
+			</div>
 		);
 	}
 });
