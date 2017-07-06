bd80b59eb68d3620e042a5255a862135546264a0
Marcelo Barrera <marcelo.barrera@ntrans.com>
2017-07-06 13:27:50 +0200

    add validation simple

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -50417,13 +50417,15 @@ var AuthorForm = React.createClass({displayName: "AuthorForm",
 					name: "firstName", 
 					label: "First Name", 
 					value: this.props.author.firstName, 
-					onChange: this.props.onChange}), 
+					onChange: this.props.onChange, 
+                    error: this.props.errors.firstName}), 
 
 				React.createElement(Input, {
 					name: "lastName", 
 					label: "Last Name", 
 					value: this.props.author.lastName, 
-					onChange: this.props.onChange}), 
+					onChange: this.props.onChange, 
+                    error: this.props.errors.lastName}), 
 
 				React.createElement("input", {type: "submit", value: "Save", className: "btn btn-default", onClick: this.props.onSave})
             )            
@@ -50520,7 +50522,8 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
 
 	getInitialState: function() {
 		return {
-			author: { id: '', firstName: '', lastName: '' }			
+			author: { id: '', firstName: '', lastName: '' },
+			errors: {}		
 		};
 	},
 
@@ -50530,9 +50533,29 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
 		this.state.author[field] = value;
 		return this.setState({author: this.state.author});
 	},
+	authorFormIsValid: function() {
+		var formIsValid = true;
+		this.state.errors = {}; //clear previous errors
+		if(this.state.author.firstName.length < 3){
+			this.state.errors.firstName = 'First name must be at least 3 characters.';
+			formIsValid = false;
+		}			
+
+		if(this.state.author.lastName.length < 3){
+			this.state.errors.lastName = 'last name must be at least 3 characters.';
+			formIsValid = false;
+		}		
+		this.setState({errors: this.state.errors});
+		
+		return formIsValid;
+
+	},
 
 	saveAuthor: function(event) {
 		event.preventDefault();
+		if(!this.authorFormIsValid()){
+			return;
+		}
 		AuthorApi.saveAuthor(this.state.author);
 		// this.setState({dirty: false});
 		toastr.success('Author saved.');
@@ -50544,7 +50567,8 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
 			React.createElement("div", null, 			
 				React.createElement(AuthorForm, {author: this.state.author, 
 				onChange: this.setAuthorState, 
-				onSave: this.saveAuthor})
+				onSave: this.saveAuthor, 
+				errors: this.state.errors})
 			)
 		);
 	}
--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -13,7 +13,8 @@ var ManageAuthorPage = React.createClass({
 
 	getInitialState: function() {
 		return {
-			author: { id: '', firstName: '', lastName: '' }			
+			author: { id: '', firstName: '', lastName: '' },
+			errors: {}		
 		};
 	},
 
@@ -23,9 +24,29 @@ var ManageAuthorPage = React.createClass({
 		this.state.author[field] = value;
 		return this.setState({author: this.state.author});
 	},
+	authorFormIsValid: function() {
+		var formIsValid = true;
+		this.state.errors = {}; //clear previous errors
+		if(this.state.author.firstName.length < 3){
+			this.state.errors.firstName = 'First name must be at least 3 characters.';
+			formIsValid = false;
+		}			
+
+		if(this.state.author.lastName.length < 3){
+			this.state.errors.lastName = 'last name must be at least 3 characters.';
+			formIsValid = false;
+		}		
+		this.setState({errors: this.state.errors});
+		
+		return formIsValid;
+
+	},
 
 	saveAuthor: function(event) {
 		event.preventDefault();
+		if(!this.authorFormIsValid()){
+			return;
+		}
 		AuthorApi.saveAuthor(this.state.author);
 		// this.setState({dirty: false});
 		toastr.success('Author saved.');
@@ -37,7 +58,8 @@ var ManageAuthorPage = React.createClass({
 			<div>			
 				<AuthorForm author={this.state.author}
 				onChange={this.setAuthorState} 
-				onSave={this.saveAuthor}/>
+				onSave={this.saveAuthor}
+				errors={this.state.errors}/>
 			</div>
 		);
 	}
